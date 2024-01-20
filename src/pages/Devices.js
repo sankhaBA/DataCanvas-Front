b54e824c65d71fbf8d186ecc8a7745e436031f44
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-01-20 20:33:51 +0530

    Fixing Device Handling Layout#60 - Added /devices  path

---
+++ src/pages/Devices.js
@@ -0,0 +1,30 @@
+// Dependencies
+import React, { useState, useEffect } from "react";
+import { } from "react-icons/fa";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+//Pages for navigation
+import { Link, useNavigate } from 'react-router-dom';
+
+// Components
+import SidebarLayout from "../components/SidebarLayout";
+import ButtonRectangle from "../components/ButtonRectangle";
+import PillButton from "../components/PillButton";
+import TextBox from "../components/TextBox";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+
+
+const Device = () => {
+
+
+    return(
+        
+        <div className="bg-black w-full h-screen">
+           
+        </div>
+    )
+}
+
+export default Device;
