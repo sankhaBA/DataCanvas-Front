16cfefc5570653f7748f59b4be49922c6511fdde
Sankha Bimsara <sankha.b21@gmail.com>
2024-01-26 01:44:05 +0530

    Fixed AB#60 Devices layout bugs fixed

--- src/components/ButtonRectangle.js
+++ src/components/ButtonRectangle.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ButtonRectangle = ({ text, onClick, icon, isPopup }) => {
     return (
-        <button className={`w-auto bg-black3 border border-green text-green rounded-md text-center ${isPopup ? 'px-5' : 'px-5 sm:px-10'} py-0.5
+        <button className={`w-auto bg-black3 text-sm border border-green text-green rounded-md text-center ${isPopup ? 'px-5' : 'px-5 sm:px-10'} py-0.5
                             hover:bg-black
                             hover:text-gray2
                             hover:border-gray1
