d1c65f74f4849c7ee78d38b5c5703591e5f581ea
Sankha Bimsara <sankha.b21@gmail.com>
2024-01-26 22:48:24 +0530

    Fixing AB#54 Insights Section added  - front end

--- src/components/InsightCard.js
+++ src/components/InsightCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const InsightCard = ({ title, subtitle, icon: Icon, onClick }) => {
     return (
         <div className={
-            `w-full sm:w-[300px] h-[150px] bg-black3 rounded-2xl my-1 sm:my-5 py-2
+            `w-full sm:w-[300px] h-[120px] sm:h-[140px] bg-black3 rounded-2xl my-1 sm:my-5 py-2 mx-5
         border border-gray1 border-opacity-60 relative overflow-hidden
         transition duration-300 hover:border-green`
         }
@@ -13,8 +13,8 @@ const InsightCard = ({ title, subtitle, icon: Icon, onClick }) => {
                     <Icon className="text-green text-3xl" />
                 </div>
                 <div className="flex flex-col flex-1 justify-center pb-5">
-                    <div className="text-gray2 text-5xl font-bold">{title}</div>
-                    <div className="text-gray2 text-lg">{subtitle}</div>
+                    <div className="text-gray2 text-3xl sm:text-4xl font-bold">{title}</div>
+                    <div className="text-gray1 text-sm sm:text-lg">{subtitle}</div>
                 </div>
                 
             </div>
--- src/pages/ProjectOverview.js
+++ src/pages/ProjectOverview.js
@@ -1,6 +1,6 @@
 // Dependencies
 import React, { useState, useEffect } from "react";
-import { } from "react-icons/fa";
+import { FaMicrochip, FaDatabase, FaClock } from "react-icons/fa";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -13,6 +13,8 @@ import ButtonRectangle from "../components/ButtonRectangle";
 import PillButton from "../components/PillButton";
 import TextBox from "../components/TextBox";
 import Spinner from "../components/Spinner";
+import InsightCard from "../components/InsightCard";
+import RectangularCard from "../components/RectangularCard";
 import axios from "axios";
 
 function ProjectOverview() {
@@ -31,12 +33,12 @@ function ProjectOverview() {
     useEffect(() => {
 
             // ---------- Getting project_id from the location state and uypdating projectID state ----------
-        try {
-            setProjectID(state.project_id);
-        } catch (err) {
-            console.log(err);
-            navigate('/projects');
-        }
+        // try {
+        //     setProjectID(state.project_id);
+        // } catch (err) {
+        //     console.log(err);
+        //     navigate('/projects');
+        // }
     }, []);
 
     useEffect(() => {
@@ -49,6 +51,17 @@ function ProjectOverview() {
         // Sidebar Layout Component
         <SidebarLayout active={0}>
 
+            {/* Page Title - Project Name */}
+            <div className={`text-lg px-7 sm:px-10 mb-3`}>UOM Weather Station</div>
+
+            {/* Insights Section */}
+            <div className={`flex flex-wrap justify-center`}>
+                <InsightCard title="59" subtitle="Registered Devices" icon={FaMicrochip} />
+                <InsightCard title="1056" subtitle="Total Data Records" icon={FaDatabase} />
+                <InsightCard title="01/01/2024" subtitle="Last Record Update" icon={FaClock} />
+            </div>
+
+            {/* Devices Section */}
         </SidebarLayout>
     );
 }
