94a67e6bf69729ced78ec718e0df7f58b3236374
Thinuri Wijesekara <wijesekarathinuri@gmail.com>
2024-03-04 21:51:26 +0530

    #83 user settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -9,101 +9,71 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 //components
-import SidebarLayout from "../components/SidebarLayout";
-import ButtonRectangle from "../components/ButtonRectangle";
-import PillButton from "../components/PillButton";
 import TextBox from "../components/TextBox";
 import Spinner from "../components/Spinner";
 import InsightCard from "../components/InsightCard";
 import RectangularCard from "../components/RectangularCard";
 import axios from "axios";
+import ScrollableContainer from "../components/ScrollableContainer";
 import NonSidebarLayout from "../components/NonSidebarLayout";
+import CriticalAction from "../components/CriticalAction";
 
 function Settings(){
     return (
+
+        
         <NonSidebarLayout>
          
           <div className=" text-white">
           <div className="overflow-y-auto h-screen absolute inset-0 bg-cover bg-center opacity-20 blur-sm "
           style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/img/projects_back_gray.png'})` }}></div>
-
+            <div className="flex flex-col justify-center mx-40 my-4 bg-black3 p-10 rounded-lg">
+            <div className="flex justify-center items-center"><div className="w-20 h-20 bg-cover rounded-full cursor-pointer flex justify-center items-center" 
+            style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/img/sample_user.jpg)` }}></div></div>
+            <h1 className="text-center my-2">John Doe</h1>
+            <div className="text-green text-center my-2">johnd@gmail</div>
 
             
-
-        
-
-<div className="flex flex-col justify-center mx-40 my-4 bg-black3 p-10 rounded-lg">
-    <div className="flex justify-center items-center"><div className="w-20 h-20 bg-cover rounded-full cursor-pointer flex justify-center items-center" 
-    style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/img/sample_user.jpg)` }}></div></div>
-    <h1 className="text-center my-2">John Doe</h1>
-    <div className="text-green text-center my-2">johnd@gmail</div>
-
-    <div className="grid grid-cols-4 md:grid-cols-8 gap-3 whitespace-nowrap text-gray1">
-        <div className="font-bold text-white">General Settings</div><div></div><div></div><div></div>
-        
+            <div className="text-m text-gray2 font-semibold mx-8 mt-8">General Settings</div>
             <div className="flex flex-row ml-8 mt-1">
-                 <div className='flex flex-col w-1/4 md:w-1/6'>
-                    <div className="text-sm md:text-md text-gray1 font-semibold mt-2">Project Name</div>
-                </div>
-                <TextBox text="" type="text" />
+                    <div className="flex flex-col w-1/4 md:w-1/6">
+                        <div className="text-sm md:text-md text-gray1 font-semibold mt-2">Your name</div>
+                    </div>
+                    <TextBox  type="text" placeholder="John Doe" maxLength={50} textAlign="left" width="w-2/3 md:w-1/4"  />
             </div>
-            <div className="flex items-center">
-                <label className="mr-2">Email</label>
-                <TextBox text="" type="text" />
+            <div className="flex flex-row ml-8 mt-1">
+                    <div className="flex flex-col w-1/4 md:w-1/6">
+                        <div className="text-sm md:text-md text-gray1 font-semibold mt-2">Email</div>
+                    </div>
+                    <TextBox  type="text" placeholder="johndoe123@gmail.com" maxLength={50} textAlign="left" width="w-2/3 md:w-1/4"  />
             </div>
+            <div className="flex flex-row ml-8 mt-1">
+                    <div className="flex flex-col w-1/4 md:w-1/6">
+                        <div className="text-sm md:text-md text-gray1 font-semibold mt-2">Change Password</div>
+                    </div>
+                    <CriticalAction buttonText={"Change Password"} buttonColor={"red"}  onClick={() => { }} />
+            </div>
+            <div className="border-t border-gray1 border-opacity-80 mx-8 my-8 mr-8"></div>
+            <div className="text-m text-gray2 font-semibold mx-8 mt-8">Critical Settings</div>
+
+            <div className="flex flex-col mx-6 mt-4">
+                <CriticalAction title="Delete your account" subtitle="This will delete all your data and your login credentials. You need to sign up and configure from scratch to use the platform again" buttonText={"Delete Account"} buttonColor={"red"} onClick={() => { }} />
+                <CriticalAction title="Privacy Policy" subtitle="All your data are protected and verified through a strong privacy policy" buttonText={"Set Policy"} buttonColor={"green"} onClick={() => { }} />
+            </div>
+            <div className="mt-8"></div>
+
+
+
         
-        <div className="my-1">
-            <label className="flex items-center">Change Password</label>
-            <ButtonRectangle text="Change" icon={FaForward} />
-        </div>
-    </div>
 
-    <div className="text-gray1 mt-4">
-        <span className="font-bold text-white">Critical Settings</span>
-        <div className="flex flex-row my-1">
-            <span className="mr-2">Delete your account</span>
-            <label>This will delete all your data and your login credentials. You need to sign up and configure from scratch to use the platform again</label>
-            <ButtonRectangle text="Delete account" />
-        </div>
-        <div className="flex flex-row my-1">
-            <span className="mr-2">Delete your account</span>
-            <label>All your data are protected and verified through a strong privacy policy</label>
-            <ButtonRectangle text="Delete account" />
-        </div>
-    </div>
-
-    <div class="grid grid-cols-4 md:grid-cols-8 gap-3">
-      <div class="flex items-center">  
-        <label class="text-white font-bold mb-1 md:mb-0 mr-2 whitespace-nowrap" for="inline-full-name">General Settings</label>
-      </div>
-      <div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-
-      <div class="flex items-center"> 
-        <label class="text-gray-500 font-bold mb-1 md:mb-0 whitespace-nowrap mr-2" for="inline-password">First Name</label>
-      </div>
-      <div class="flex items-center col-span-3">
-        <ButtonRectangle text="Change password" icon={FaForward} />
-      </div>
-        <div></div><div></div><div></div><div></div><div></div><div></div>
-      
-      
-    </div>
-
-
-</div>
-</div>
-
-
-
-
-<div class="grid grid-cols-4 md:grid-cols-8 gap-3"> 
-      <div className="flex items-center">
-        <label className="text-white">Your Name</label>
-      </div>
-</div>
-
+  
+  
+         </div>
+         </div>
          
         </NonSidebarLayout>
+        
+        
 
     );
 }
