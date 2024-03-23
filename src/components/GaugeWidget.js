3fff982f567e0b35d2f5e92238594f6c0b273b22
Thinuri Wijesekara <wijesekarathinuri@gmail.com>
2024-03-23 21:45:07 +0530

    AB#99 added gauge widget as a component

---
+++ src/components/GaugeWidget.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { FaTools, FaUikit } from "react-icons/fa";
+import PopupContainer from "./PopupContainer";
+import ButtonRectangle from "./ButtonRectangle";
+import TextBox from "./TextBox";
+import SelectBox from "./SelectBox";
+import PillButton from "./PillButton"
+
+const GaugeWidget = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  // Function to handle closing the popup
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  return (
+    <div>
+      <ButtonRectangle onClick={openPopup}>Open Popup</ButtonRectangle>
+      {/* PopupContainer is rendered conditionally based on the state of isPopupOpen */}
+      {isPopupOpen && (
+        <PopupContainer
+          title="Configure Widget"
+          Icon={FaTools}
+          isOpen={isPopupOpen} 
+          onClose={closePopup} 
+          closeIconVisible={true}
+        >
+          {/* Content of the popup */}
+          <div className="my-3">
+            <span className="text-sm">Field Name</span>
+            
+          <SelectBox label="Select" options={["Temperature", "Option 2"]}  />
+          <div style={{ paddingBottom: "15px" }}></div>
+
+          <span className="text-sm">Maximum Value</span>
+          <TextBox type="number" placeholder="Enter maximum value" />
+          <div style={{ paddingBottom: "30px" }}></div>
+
+          <div className="flex justify-center space-x-4">
+          <ButtonRectangle text="Done" Icon="Fatick" onClick={closePopup}/>
+          </div>
+          </div>
+        </PopupContainer>
+      )}
+    </div>
+  );
+};
+
+export default GaugeWidget;
--- src/pages/SamplePage.js
+++ src/pages/SamplePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SidebarLayout from '../components/SidebarLayout';
 import LoginPopup from '../components/LoginPopup';
+import GaugeWidget from '../components/GaugeWidget';
 
 function SamplePage() {
     return (
-        <SidebarLayout active={3} addressText={''}>
-            <LoginPopup isOpen={true} closeFunction={() => { }} setAuthenticationResult={() => { }} email={''} />
-        </SidebarLayout>
+        // <SidebarLayout active={3} addressText={''}>
+        //     <LoginPopup isOpen={true} closeFunction={() => { }} setAuthenticationResult={() => { }} email={''} />
+        // </SidebarLayout>
+        <GaugeWidget    />
     );
 }
 
