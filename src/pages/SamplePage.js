51c02f5112d200e2d1202fcfc8a336c1070696a1
Sankha Bimsara <sankha.b21@gmail.com>
2024-03-25 15:48:34 +0530

    Fixed AB#101 small changes done

--- src/pages/SamplePage.js
+++ src/pages/SamplePage.js
@@ -2,12 +2,12 @@ import React from 'react';
 import SidebarLayout from '../components/SidebarLayout';
 import LoginPopup from '../components/LoginPopup';
 import AddWidgetPopup from '../components/AddWidgetPopup';
-import AddWidgetChart from '../components/AddWidgetChart';
+import AddWidgetChart from '../components/AddChartPopup';
 
 function SamplePage() {
     return (
         <SidebarLayout active={3} addressText={''}>
-         <AddWidgetChart isOpen={true} closeFunction={() => { }} />
+            <AddWidgetChart isOpen={true} closeFunction={() => { }} />
         </SidebarLayout>
     );
 }
