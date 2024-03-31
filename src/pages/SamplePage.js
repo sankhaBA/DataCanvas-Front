af3151f3d76dd255c944fea655de160577bfe901
Sankha Bimsara <sankha.b21@gmail.com>
2024-03-31 19:29:55 +0530

    error fixed

--- src/pages/SamplePage.js
+++ src/pages/SamplePage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import SidebarLayout from '../components/layouts/SidebarLayout';
-import AddToggleWidgetPopup from '../components/widgets/AddToggleWidgetPopup';
+
 
 function SamplePage() {
     return (
         <SidebarLayout active={3} breadcrumb={''}>
-            <AddToggleWidgetPopup isOpen={true} closeFunction={() => { }} />
+
         </SidebarLayout>
     );
 }
